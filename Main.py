c492276d60d8f356378fdbb8b1f4c5e8237dea5b
PunchPotato <119630204+PunchPotato@users.noreply.github.com>
2023-07-14 09:34:54 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,59 +1,75 @@
-# Import packages
-import customtkinter
-import tkinter
+import tkinter as tk
 from tkinter import font
-
-customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("dark-blue")
-
-root = customtkinter.CTk()
-root.geometry(f"{720}x{980}")
-custom_font = font.Font(family="typewrite", size=70, weight="normal")
+from tkinter import *
+from PIL import ImageTk, Image
 
 
-def login_page():
-    after_frame.pack_forget()
-    login_frame.pack()
+class LoginPage(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.geometry('720x980')
+        self.title('Login Page')
+        self.custom_font = font.Font(family="typewriter", size=70, weight="normal")
+        self.open_eye = None
+        self.close_eye = None
+        self.initialize_widgets()
+
+    def initialize_widgets(self):
+        self.bg_image = ImageTk.PhotoImage(Image.open("Login page/trial 2.png"))
+        self.bg_label = tk.Label(self, image=self.bg_image)
+        self.bg_label.place(y=0, x=0)
+
+        login_title = tk.Label(self, text='USER LOGIN', font=self.custom_font, bg='white', fg='#0d2158')
+        login_title.place(y=200, x=60)
+
+        self.name_entry = tk.Entry(self, font=("typewrite", 20, "normal"), fg="grey", bd=0)
+        self.name_entry.insert(0, "Username")
+        self.name_entry.place(y=350, x=220)
+        self.name_entry.bind("<FocusIn>", self.temp_name_entry_text)
+
+        self.password_entry = tk.Entry(self, font=("typewrite", 20, "normal"), fg="grey", bd=0)
+        self.password_entry.insert(0, "Password")
+        self.password_entry.place(y=450, x=220)
+        self.password_entry.bind("<FocusIn>", self.temp_password_entry_text)
+
+        self.open_eye = ImageTk.PhotoImage(Image.open('Login page/openeye.png'))
+        self.close_eye = ImageTk.PhotoImage(Image.open('Login page/closeeye.png'))
+        self.eye_button = Button(self, image=self.open_eye, bd=0, bg='white', activebackground='white', cursor='hand2',
+                                 command=self.hide)
+        self.eye_button.place(y=450, x=500)
+
+        self.forget_button = Button(self, text='Forgot Password?', bd=0, bg='white', activebackground='white',
+                                    cursor='hand2', font=("typewrite", 9, "bold"), fg='firebrick1',
+                                    activeforeground='firebrick1')
+        self.forget_button.place(x=410, y=495)
+
+        tk.Frame(self, width=300, height=2, bg="#0d2158").place(y=390, x=220)
+        tk.Frame(self, width=300, height=2, bg="#0d2158").place(y=490, x=220)
+
+        login_button = tk.Button(self, text="Log in", font=("typewrite", 20, "bold"),  bd=0, bg='#0d2158',
+                                 activebackground='#0d2158', cursor='hand2', fg="white", width=19)
+        login_button.place(y=550, x=210)
+
+        or_label = Label(self, text='----------------------------OR----------------------------', font=("typewrite", 20)
+                         , fg='#0d2158', bg='white')
+        or_label.place(x=100, y=620)
+
+    def temp_name_entry_text(self, event):
+        self.name_entry.delete(0, "end")
+
+    def temp_password_entry_text(self, event):
+        self.password_entry.delete(0, "end")
+
+    def hide(self):
+        self.password_entry.config(show='*')
+        self.eye_button.config(image=self.close_eye, command=self.show)
+
+    def show(self):
+        self.password_entry.config(show='')
+        self.eye_button.config(image=self.open_eye, command=self.hide)
 
 
-def after_page():
-    login_frame.pack_forget()
-    after_frame.pack()
+if __name__ == "__main__":
+    login_page = LoginPage()
+    login_page.mainloop()
 
-
-# page 1
-login_frame = customtkinter.CTkFrame(master=root)
-login_frame.pack(pady=20, padx=60, fill="both", expand=True)
-
-label = tkinter.Label(master=login_frame, text="Login", font=custom_font, fg="#1f538d",
-                           bg="#212121")
-label.pack(pady=120, padx=10)
-
-entry_1 = customtkinter.CTkEntry(master=login_frame, placeholder_text="Username", width=300, height=30
-                                      )
-entry_1.pack(pady=12, padx=10)
-
-entry_2 = customtkinter.CTkEntry(master=login_frame, placeholder_text="Password", show="*", width=300,
-                                      height=30)
-entry_2.pack(pady=30, padx=10)
-
-button = customtkinter.CTkButton(master=login_frame, text="Login", command=after_page)
-button.pack(pady=30, padx=10)
-
-checkbox = customtkinter.CTkCheckBox(master=login_frame, text="Remember Me")
-checkbox.pack(pady=160, padx=10)
-
-# page 2
-after_frame = customtkinter.CTkFrame(master=root)
-after_frame.pack(pady=20, padx=60, fill="both", expand=True)
-
-label = tkinter.Label(master=after_frame, text="you logged in", font=custom_font, fg="#1f538d",
-                           bg="#212121")
-label.pack(pady=120, padx=10)
-
-button = customtkinter.CTkButton(master=after_frame, text="back", command=login_page)
-button.pack(pady=30, padx=10)
-
-login_page()
-
-root.mainloop()
