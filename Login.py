a8bf29f6bcf5731c0e041f5d9cac8b90ba2162c5
PunchPotato <119630204+PunchPotato@users.noreply.github.com>
2023-07-20 23:19:41 +0100

    Update Login.py

--- Login.py
+++ Login.py
@@ -2,7 +2,8 @@ import tkinter as tk
 from tkinter import font
 from tkinter import *
 from PIL import ImageTk, Image
-
+from tkinter import messagebox
+import pymysql
 
 
 class LoginPage(tk.Tk):
@@ -16,12 +17,12 @@ class LoginPage(tk.Tk):
         self.initialize_widgets()
 
     def initialize_widgets(self):
-        self.bg_image = ImageTk.PhotoImage(Image.open("Login page/trial 2.png"))
+        self.bg_image = ImageTk.PhotoImage(Image.open("Login page/another one.png"))
         self.bg_label = tk.Label(self, image=self.bg_image)
         self.bg_label.place(y=0, x=0)
 
         login_title = tk.Label(self, text='USER LOGIN', font=self.custom_font, bg='white', fg='#0d2158')
-        login_title.place(y=200, x=70)
+        login_title.place(y=100, x=70)
 
         self.name_entry = tk.Entry(self, font=("typewriter", 20, "normal"), fg="grey", bd=0)
         self.name_entry.insert(0, "Username")
@@ -48,7 +49,8 @@ class LoginPage(tk.Tk):
         tk.Frame(self, width=300, height=2, bg="#0d2158").place(y=490, x=220)
 
         login_button = tk.Button(self, text="Log in", font=("typewriter", 20, "bold"), bd=0, bg='#0d2158',
-                                 activebackground='#0d2158', cursor='hand2', fg="white", width=19)
+                                 activebackground='#0d2158', cursor='hand2', fg="white", width=19,
+                                 command=self.connect_database)
         login_button.place(y=550, x=210)
 
         or_label = Label(self, text='----------------------------OR----------------------------',
@@ -77,10 +79,12 @@ class LoginPage(tk.Tk):
         self.newaccountButton.place(x=380, y=750)
 
     def temp_name_entry_text(self, event):
-        self.name_entry.delete(0, "end")
+        if self.name_entry.get() == 'Username':
+            self.name_entry.delete(0, "end")
 
     def temp_password_entry_text(self, event):
-        self.password_entry.delete(0, "end")
+        if self.password_entry.get() == 'Password':
+            self.password_entry.delete(0, "end")
 
     def hide(self):
         self.password_entry.config(show='*')
@@ -94,6 +98,28 @@ class LoginPage(tk.Tk):
         self.destroy()
         import Signup
         Signup.SignupPage().mainloop()
+
+    def connect_database(self):
+        if self.name_entry.get() == '' or self.password_entry.get() == '':
+            messagebox.showerror("Error", "All fields must be filled.")
+        elif self.name_entry.get() == 'Username' or self.password_entry.get() == 'Password':
+            messagebox.showerror("Error", "All fields must be filled.")
+        else:
+            try:
+                con = pymysql.connect(host='localhost', user='root', password='Creatinemonohydrate04!')
+                my_curser = con.cursor()
+            except:
+                messagebox.showerror("Error", "Failed to connect to database")
+                return
+            query = 'use mydatabase'
+            my_curser.execute(query)
+            query = 'select * from user_data where username =%s and password =%s'
+            my_curser.execute(query, (self.name_entry.get(), self.password_entry.get()))
+            row = my_curser.fetchone()
+            if row == None:
+                messagebox.showerror("Error", "Invalid username or password.")
+            else:
+                messagebox.showinfo("Success", "Login is successful.")
 
 
 if __name__ == "__main__":
